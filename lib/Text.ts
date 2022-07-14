0d4c75865e7351fa530125d3e03a50c392b58591
Konstantin Kolesnikov <kosstanley@gmail.com>
2022-07-14 23:04:15 +0400

    Text geometry.

---
+++ lib/Text.ts
@@ -0,0 +1,161 @@
+import * as T from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { FontLoader, Font } from 'three/examples/jsm/loaders/FontLoader'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry'
+import gsap from 'gsap'
+import * as dat from 'dat.gui'
+import envTexture1 from 'lib/textures/px.jpg'
+import envTexture2 from 'lib/textures/nx.jpg'
+import envTexture3 from 'lib/textures/py.jpg'
+import envTexture4 from 'lib/textures/ny.jpg'
+import envTexture5 from 'lib/textures/pz.jpg'
+import envTexture6 from 'lib/textures/nz.jpg'
+
+export default class Text implements IApp {
+  private sizes = { width: 0, height: 0 }
+  private colors = {
+    mesh: 0xff0000,
+    ambientLight: 0x222222,
+    pointLight: 0xffffff,
+  }
+  private background = new T.Color('rgb(38, 38, 38)')
+  private renderer: T.WebGLRenderer
+  private controls: OrbitControls
+  private scene: T.Scene
+  private group: T.Group
+  private camera: T.PerspectiveCamera
+  private mesh?: T.Mesh
+  private geometry?: TextGeometry
+  private material: T.Material
+  private axesHelper: T.AxesHelper
+  private clock: T.Clock
+  private loadingManager: T.LoadingManager
+  private cubeTextureLoader: T.CubeTextureLoader
+  private fontLoader: FontLoader
+
+  constructor(public container: HTMLElement = document.body) {
+    this.resetSizes()
+    this.scene = new T.Scene()
+    this.group = new T.Group()
+    this.camera = new T.PerspectiveCamera(
+      40,
+      this.sizes.width / this.sizes.height,
+      1,
+      1000
+    )
+    this.renderer = new T.WebGLRenderer({
+      antialias: true,
+    })
+    this.loadingManager = new T.LoadingManager()
+    this.cubeTextureLoader = new T.CubeTextureLoader()
+
+    this.fontLoader = new FontLoader(this.loadingManager)
+    this.fontLoader.load('/fonts/helvetiker_regular.typeface.json', font => {
+      this.geometry = new TextGeometry('Hello world!', {
+        font,
+        size: 0.5,
+        height: 0.2,
+        curveSegments: 5,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelSegments: 4,
+      })
+      this.geometry.computeBoundingBox()
+      this.geometry.translate(
+        -this.geometry.boundingBox!.max.x * 0.5,
+        -this.geometry.boundingBox!.max.y * 0.5,
+        -this.geometry.boundingBox!.max.z * 0.5
+      )
+      this.mesh = new T.Mesh(this.geometry, this.material)
+      this.group.add(this.mesh)
+      this.scene.add(this.group).add(this.camera).add(this.axesHelper)
+    })
+    this.material = new T.MeshStandardMaterial({
+      metalness: 0.5,
+      roughness: 0.05,
+      envMap: this.cubeTextureLoader.load([
+        envTexture1.src,
+        envTexture2.src,
+        envTexture3.src,
+        envTexture4.src,
+        envTexture5.src,
+        envTexture6.src,
+      ]),
+      color: this.colors.mesh,
+      opacity: 1,
+      transparent: true,
+    })
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.clock = new T.Clock()
+    this.axesHelper = new T.AxesHelper(0)
+    this.camera.position.set(1, 1, 6)
+    this.controls.maxPolarAngle = Math.PI
+    this.controls.minDistance = 5
+    this.controls.maxDistance = 5
+    this.controls.rotateSpeed = 2
+    this.controls.enablePan = false
+    this.controls.enableDamping = true
+    this.controls.update()
+    this.scene.background = this.background
+    this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+
+    this.setupLighting()
+    // this.setupDatGui()
+    this.setupEventListeners()
+  }
+
+  private animate = () => {
+    requestAnimationFrame(this.animate)
+
+    this.group.rotation.y += 0.01
+    this.group.position.y = Math.sin(this.clock.getElapsedTime()) / 10
+
+    this.controls.update()
+    this.renderer.render(this.scene, this.camera)
+  }
+
+  private onWindowResize = () => {
+    this.resetSizes()
+    this.camera.aspect = this.sizes.width / this.sizes.height
+    this.camera.updateProjectionMatrix()
+    this.renderer.setSize(this.sizes.width, this.sizes.height)
+  }
+
+  private onDoubleClick = () => {
+    if (!document.fullscreenEnabled) {
+      return
+    }
+
+    if (document.fullscreenElement) {
+      return document.exitFullscreen()
+    }
+
+    return this.renderer.domElement.requestFullscreen()
+  }
+
+  private resetSizes() {
+    this.sizes = { width: window.innerWidth, height: window.innerHeight }
+  }
+
+  private setupLighting() {
+    const ambientLight = new T.AmbientLight(0xffffff, 0.5)
+    const pointLight = new T.PointLight(0xffffff, 0.5)
+    pointLight.position.x = 2
+    pointLight.position.y = 3
+    pointLight.position.z = 4
+    this.scene.add(ambientLight)
+    this.scene.add(pointLight)
+  }
+
+  private setupEventListeners() {
+    window.addEventListener('resize', this.onWindowResize)
+    window.addEventListener('dblclick', this.onDoubleClick)
+  }
+
+  render(): void {
+    this.container.appendChild(this.renderer.domElement)
+    this.animate()
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import { useEffect } from 'react'
-import App from '@lib/Box'
+import App from '@lib/Text'
 
 const Home: NextPage = () => {
   useEffect(() => {
